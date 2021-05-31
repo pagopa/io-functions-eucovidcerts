3528b0d138383c4fb1a7020a597469737739f7a9
fabriziopapi <fabrizio.papi@pagopa.it>
2021-05-31 17:31:13 +0200

    Add SubmitMessageForUser dummy Endpoint
    Co-authored-by: gquadrati <gquadrati@users.noreply.github.com>

---
+++ SubmitMessageForUser/index.ts
@@ -0,0 +1,25 @@
+import { AzureFunction, Context } from "@azure/functions";
+import * as express from "express";
+import { secureExpressApp } from "@pagopa/io-functions-commons/dist/src/utils/express";
+import { setAppContext } from "@pagopa/io-functions-commons/dist/src/utils/middlewares/context_middleware";
+import createAzureFunctionHandler from "@pagopa/express-azure-functions/dist/src/createAzureFunctionsHandler";
+import { getSubmitMessageForUserAsExpressHandler } from "./handler";
+
+// Setup Express
+const app = express();
+secureExpressApp(app);
+
+// Add express route
+app.post(
+  "/api/v1/messages/:fiscalcode?",
+  getSubmitMessageForUserAsExpressHandler()
+);
+
+const azureFunctionHandler = createAzureFunctionHandler(app);
+
+const httpStart: AzureFunction = (context: Context): void => {
+  setAppContext(app, context);
+  azureFunctionHandler(context);
+};
+
+export default httpStart;
