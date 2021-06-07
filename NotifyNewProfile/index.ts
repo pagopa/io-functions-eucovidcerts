23184d5725db6050ed232ff3223d64902a47e3a2
Daniele Manni <dan.manni@gmail.com>
2021-06-07 10:35:25 +0200

    [#IPV-64] Refactor with configurable https fetch

--- NotifyNewProfile/index.ts
+++ NotifyNewProfile/index.ts
@@ -1,12 +1,11 @@
 import { AzureFunction } from "@azure/functions";
-import { getFetch } from "@pagopa/ts-commons/lib/agent";
 import { getConfigOrThrow } from "../utils/config";
 import { getDGCClientBuilder } from "../utils/dgcClient";
 import { NotifyNewProfile } from "./handler";
 
 const config = getConfigOrThrow();
 
-const clientBuilder = getDGCClientBuilder(config, getFetch(process.env));
+const clientBuilder = getDGCClientBuilder(config, process.env);
 
 const index: AzureFunction = NotifyNewProfile(clientBuilder);
 
--- utils/dgcClient.ts
+++ utils/dgcClient.ts
@@ -2,20 +2,30 @@ import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 import { Client as IDGCClient, createClient } from "../generated/dgc/client";
 import { IConfig } from "./config";
 import { sha256 } from "./conversions";
+import { DGCEnvironment, getFetchWithClientCertificate } from "./httpsAgent";
 
 export type IGetDGCClient = (fiscalCodeHash: NonEmptyString) => IDGCClient;
 
 export const getDGCClientBuilder: (
   config: IConfig,
-  fetchApi: typeof fetch
-) => IGetDGCClient = (config, fetchApi) => (
+  env: NodeJS.ProcessEnv
+) => IGetDGCClient = (config, env) => (
   fiscalCodeHash: NonEmptyString
 ): IDGCClient => {
   if (config.DGC_LOAD_TEST_FISCAL_CODES.map(sha256).includes(fiscalCodeHash)) {
-    return createClient({ baseUrl: config.DGC_LOAD_TEST_URL.href, fetchApi });
+    return createClient({
+      baseUrl: config.DGC_LOAD_TEST_URL.href,
+      fetchApi: getFetchWithClientCertificate(env, DGCEnvironment.LOAD_TEST)
+    });
   }
   if (config.DGC_UAT_FISCAL_CODES.map(sha256).includes(fiscalCodeHash)) {
-    return createClient({ baseUrl: config.DGC_UAT_URL.href, fetchApi });
+    return createClient({
+      baseUrl: config.DGC_UAT_URL.href,
+      fetchApi: getFetchWithClientCertificate(env, DGCEnvironment.UAT)
+    });
   }
-  return createClient({ baseUrl: config.DGC_PROD_URL.href, fetchApi });
+  return createClient({
+    baseUrl: config.DGC_PROD_URL.href,
+    fetchApi: getFetchWithClientCertificate(env, DGCEnvironment.PROD)
+  });
 };
