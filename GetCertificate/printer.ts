ccd6d2e9fd41714247af116780b3e86b1af359d7
fabriziopapi <fabrizio.papi@pagopa.it>
2021-06-09 12:43:09 +0200

    add printers implementations

--- GetCertificate/printer.ts
+++ GetCertificate/printer.ts
@@ -1,87 +1,122 @@
+/* eslint-disable sort-keys */
 import {
   PreferredLanguage,
   PreferredLanguageEnum
 } from "@pagopa/io-functions-commons/dist/generated/definitions/PreferredLanguage";
-import { toString } from "fp-ts/lib/function";
+import * as o from "fp-ts/lib/Option";
+import { match } from "ts-pattern";
 import {
   Certificates,
   TestCertificate,
   VacCertificate,
-  VaccinationEntry
+  VaccinationEntry,
+  TestEntry,
+  RecoveryCertificate
 } from "./certificate";
-import * as vacDetailsEn from "./markdown files/eucovidcertDetailsVaccinationEn";
-import * as vacDetailsIt from "./markdown files/eucovidcertDetailsVaccinationIt";
-import * as testDetailsIt from "./markdown files/eucovidcertDetailsTestIt";
-import * as testDetailsEn from "./markdown files/eucovidcertDetailsTestEn";
-import * as infoEn from "./markdown files/eucovidcertInfoVaccinationEn";
-import * as infoIt from "./markdown files/eucovidcertInfoVaccinationIt";
+import * as vacDetailsEn from "./markdown/eucovidcertDetailsVaccinationEn";
+import * as vacDetailsIt from "./markdown/eucovidcertDetailsVaccinationIt";
+import * as testDetailsIt from "./markdown/eucovidcertDetailsTestIt";
+import * as testDetailsEn from "./markdown/eucovidcertDetailsTestEn";
+import * as vacInfoEn from "./markdown/eucovidcertInfoVaccinationEn";
+import * as vacInfoIt from "./markdown/eucovidcertInfoVaccinationIt";
 
-const assertNever = (x: never): never => {
-  throw new Error(`Unexpected object: ${toString(x)}`);
+// const assertNever = (x: never): never => {
+//   throw new Error(`Unexpected object: ${toString(x)}`);
+// };
+
+interface IPrintersForLanguage {
+  readonly detailVaccinePrinter: (v: VaccinationEntry) => string;
+  readonly detailTestPrinter: (t: TestEntry) => string;
+  readonly detailRecoveryPrinter: (r: unknown) => string;
+  readonly infoVaccinePrinter: (v: VaccinationEntry) => string;
+  readonly infoTestPrinter: (t: TestEntry) => string;
+  readonly infoRecoveryPrinter: (r: unknown) => string;
+}
+
+const printersConfigurations = new Map<PreferredLanguage, IPrintersForLanguage>(
+  [
+    [
+      PreferredLanguageEnum.it_IT,
+      {
+        detailVaccinePrinter: vacDetailsIt.getDetailPrinter,
+        detailTestPrinter: testDetailsIt.getDetailPrinter,
+        detailRecoveryPrinter: (): string => "", // TODO
+        infoVaccinePrinter: vacInfoIt.getInfoPrinter,
+        infoTestPrinter: (): string => "", // TODO
+        infoRecoveryPrinter: (): string => "" // TODO
+      }
+    ],
+    [
+      PreferredLanguageEnum.en_GB,
+      {
+        detailVaccinePrinter: vacDetailsEn.getDetailPrinter,
+        detailTestPrinter: testDetailsEn.getDetailPrinter,
+        detailRecoveryPrinter: (): string => "", // TODO
+        infoVaccinePrinter: vacInfoEn.getInfoPrinter,
+        infoTestPrinter: (): string => "", // TODO
+        infoRecoveryPrinter: (): string => "" // TODO
+      }
+    ]
+  ]
+);
+
+export const defaultPrinter: IPrintersForLanguage = {
+  detailVaccinePrinter: vacDetailsEn.getDetailPrinter,
+  detailTestPrinter: testDetailsEn.getDetailPrinter,
+  detailRecoveryPrinter: () => "", // TODO
+  infoVaccinePrinter: vacInfoEn.getInfoPrinter,
+  infoTestPrinter: () => "", // TODO
+  infoRecoveryPrinter: () => "" // TODO
 };
 
-/**
- * Signature of a function that creates a text from a Certificate object
- */
-export type CertificatePrinter = (e: Certificates) => string;
+export const getPrinterForLanguage = (
+  lang: o.Option<PreferredLanguage>
+): IPrintersForLanguage =>
+  lang
+    .chain(l => o.fromNullable(printersConfigurations.get(l)))
+    .getOrElse(defaultPrinter);
 
-export const createDetailsPrinter = (
-  lang: PreferredLanguage
-): CertificatePrinter => (e: Certificates): string => {
-  // the first certificate in the QR Code will be used to compile the markdown. The others will be ignored.
+export const printDetails = (
+  lang: o.Option<PreferredLanguage>,
+  c: Certificates
+): string =>
+  match(c)
+    .when(VacCertificate.is, vc =>
+      getPrinterForLanguage(lang).detailVaccinePrinter(vc.v[0])
+    )
+    .when(TestCertificate.is, tc =>
+      getPrinterForLanguage(lang).detailTestPrinter(tc.t[0])
+    )
+    .when(RecoveryCertificate.is, rc =>
+      getPrinterForLanguage(lang).detailRecoveryPrinter(rc.r[0])
+    )
+    .exhaustive();
 
-  if (VacCertificate.is(e)) {
-    const certificate = e.v[0];
-    switch (lang) {
-      case PreferredLanguageEnum.it_IT:
-        return vacDetailsIt.getDetailPrinter(certificate);
-      case PreferredLanguageEnum.en_GB:
-      case PreferredLanguageEnum.de_DE:
-      case PreferredLanguageEnum.fr_FR:
-      case PreferredLanguageEnum.es_ES:
-        return vacDetailsEn.getDetailPrinter(certificate);
-      default:
-        assertNever(lang);
-    }
-  } else if (TestCertificate.is(e)) {
-    const testCertificate = e.t[0];
-    switch (lang) {
-      case PreferredLanguageEnum.it_IT:
-        return testDetailsIt.getDetailPrinter(testCertificate);
-      case PreferredLanguageEnum.en_GB:
-      case PreferredLanguageEnum.de_DE:
-      case PreferredLanguageEnum.fr_FR:
-      case PreferredLanguageEnum.es_ES:
-        return testDetailsEn.getDetailPrinter(testCertificate);
-      default:
-        assertNever(lang);
-    }
-  }
-  throw Error("No markdown generator found for current certificate");
-};
+export const printInfo = (
+  lang: o.Option<PreferredLanguage>,
+  c: Certificates
+): string =>
+  match(c)
+    .when(VacCertificate.is, vc =>
+      getPrinterForLanguage(lang).infoVaccinePrinter(vc.v[0])
+    )
+    .when(TestCertificate.is, tc =>
+      getPrinterForLanguage(lang).infoTestPrinter(tc.t[0])
+    )
+    .when(RecoveryCertificate.is, rc =>
+      getPrinterForLanguage(lang).infoRecoveryPrinter(rc.r[0])
+    )
+    .exhaustive();
 
-export const createInfoPrinter = (
-  lang: PreferredLanguage
-): CertificatePrinter => (e: Certificates): string => {
-  // the first certificate in the QR Code will be used to compile the markdown. The others will be ignored.
-
-  if (VacCertificate.is(e)) {
-    const certificate = e.v[0];
-    switch (lang) {
-      case PreferredLanguageEnum.it_IT:
-        return infoIt.getInfoPrinter(certificate);
-      case PreferredLanguageEnum.en_GB:
-      case PreferredLanguageEnum.de_DE:
-      case PreferredLanguageEnum.fr_FR:
-      case PreferredLanguageEnum.es_ES:
-        return infoEn.getInfoPrinter(certificate);
-      default:
-        assertNever(lang);
-    }
-  }
-
-  throw Error("No markdown generator found for current certificate");
-};
+export const printUvci = (
+  _lang: o.Option<PreferredLanguage>,
+  c: Certificates
+): string =>
+  match(c)
+    .when(VacCertificate.is, cv => cv.v[0].ci)
+    .when(TestCertificate.is, ct => ct.t[0].ci)
+    .when(RecoveryCertificate.is, cr => cr.r[0].ci)
+    .exhaustive(); // TODO
 
 export const isVaccinationProcessEnded = (v: VaccinationEntry): boolean =>
   v.dn === v.sd;
