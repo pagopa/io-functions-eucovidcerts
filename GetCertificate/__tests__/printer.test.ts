e6f1e8d8336c31610c88abbb731f2ed4efa39cf6
gquadrati <greta.quadrati@pagopa.it>
2021-06-10 10:49:38 +0200

    [#221] use constant in printer tests

--- GetCertificate/__tests__/printer.test.ts
+++ GetCertificate/__tests__/printer.test.ts
@@ -1,20 +1,27 @@
-import {
-  PreferredLanguage,
-  PreferredLanguageEnum
-} from "@pagopa/io-functions-commons/dist/generated/definitions/PreferredLanguage";
-import { some } from "fp-ts/lib/Option";
+import { PreferredLanguageEnum } from "@pagopa/io-functions-commons/dist/generated/definitions/PreferredLanguage";
 
-import { Certificates } from "../certificate";
-import { printDetails, printInfo, formatCertificateIssuer } from "../printer";
+import {
+  formatCertificateIssuer,
+  ITALY_HEALTHCARE_ISSUER,
+  HEALTHCARE_DEP_IT,
+  HEALTHCARE_DEP_EN
+} from "../printer";
 
 describe("Printer", () => {
   it("should format Certificate Issuer with Ministero della salute if value is IT and language it_IT", () => {
-    const result = formatCertificateIssuer("IT", PreferredLanguageEnum.it_IT);
-    expect(result).toEqual("Ministero della Salute");
+    const result = formatCertificateIssuer(
+      ITALY_HEALTHCARE_ISSUER,
+      PreferredLanguageEnum.it_IT
+    );
+    expect(result).toEqual(HEALTHCARE_DEP_IT);
   });
-  it("should format Certificate Issuer with Ministry of Health if value is IT and language it_IT", () => {
-    const result = formatCertificateIssuer("IT", PreferredLanguageEnum.en_GB);
-    expect(result).toEqual("Ministry of Health");
+
+  it("should format Certificate Issuer with Ministry of Health if value is IT and language not it_IT", () => {
+    const result = formatCertificateIssuer(
+      ITALY_HEALTHCARE_ISSUER,
+      PreferredLanguageEnum.fr_FR
+    );
+    expect(result).toEqual(HEALTHCARE_DEP_EN);
   });
   it("should format Certificate Issuer with its value if value is different from IT and language it_IT", () => {
     const result = formatCertificateIssuer(
