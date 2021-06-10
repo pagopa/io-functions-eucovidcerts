091785acddf129ea09e784f8a7b7a09e0a82d890
gquadrati <greta.quadrati@pagopa.it>
2021-06-10 09:51:26 +0200

    [#221] translate test result values

--- GetCertificate/valuesets/labTestTypes.ts
+++ GetCertificate/valuesets/labTestTypes.ts
@@ -12,10 +12,7 @@ export const labTestTypes = new Map<string, ITranslatable>([
     {
       id: "LP6464-4",
       displays: new Map<PreferredLanguage, string>([
-        [
-          PreferredLanguageEnum.it_IT,
-          "Nucleic acid amplification with probe detection"
-        ],
+        [PreferredLanguageEnum.it_IT, "Test molecolare"],
         [
           PreferredLanguageEnum.en_GB,
           "Nucleic acid amplification with probe detection"
@@ -28,7 +25,7 @@ export const labTestTypes = new Map<string, ITranslatable>([
     {
       id: "LP217198-3",
       displays: new Map<PreferredLanguage, string>([
-        [PreferredLanguageEnum.it_IT, "Rapid immunoassay"],
+        [PreferredLanguageEnum.it_IT, "Test antigenico rapido"],
         [PreferredLanguageEnum.en_GB, "Rapid immunoassay"]
       ])
     }
