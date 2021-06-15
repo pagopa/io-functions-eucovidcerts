481d5bd10d4fc44e426cb196b10c3fdefa21e468
gquadrati <greta.quadrati@pagopa.it>
2021-06-15 18:43:30 +0200

    fix lint and tests

--- GetCertificate/markdown/eucovidcertDetailsVaccinationEn.ts
+++ GetCertificate/markdown/eucovidcertDetailsVaccinationEn.ts
@@ -12,10 +12,11 @@ export const getDetailPrinter = (v: VaccinationEntry): string =>
   `
 ## Vaccination Certificate  
 ***
-${isVaccinationProcessEnded(v)
+${
+  isVaccinationProcessEnded(v)
     ? "**Certification valid for 270 days (9 months) from the date of the last administration**"
     : "**Certification valid until next dose**"
-  }
+}
 ***
 
 Disease or agent targeted  
