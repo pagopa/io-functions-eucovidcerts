fa7d8b1cf5b15e93cb70612fe3b5775cd8dd18e9
gquadrati <greta.quadrati@pagopa.it>
2021-06-15 12:34:06 +0200

    [#IP-241] add applicationinsights

--- SubmitMessageForUser/index.ts
+++ SubmitMessageForUser/index.ts
@@ -1,18 +1,25 @@
 import { AzureFunction, Context } from "@azure/functions";
 import * as express from "express";
+import { TelemetryClient } from "applicationinsights";
+
 import { secureExpressApp } from "@pagopa/io-functions-commons/dist/src/utils/express";
 import { setAppContext } from "@pagopa/io-functions-commons/dist/src/utils/middlewares/context_middleware";
 import createAzureFunctionHandler from "@pagopa/express-azure-functions/dist/src/createAzureFunctionsHandler";
 import { getFetch } from "@pagopa/ts-commons/lib/agent";
 import { getConfigOrThrow } from "../utils/config";
 import { createClient } from "../utils/serviceClient";
+import { initTelemetryClient } from "../utils/appinsights";
 import { getSubmitMessageForUserHandler } from "./handler";
+
+const config = getConfigOrThrow();
 
 // Setup Express
 const app = express();
 secureExpressApp(app);
 
-const config = getConfigOrThrow();
+// Setup Appinsight
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+const telemetryClient = initTelemetryClient(config) as TelemetryClient;
 
 const fetchApi = getFetch(process.env);
 const serviceClient = createClient(
@@ -21,7 +28,10 @@ const serviceClient = createClient(
   config.FNSERVICES_API_KEY
 );
 
-app.post("/api/v1/messages", getSubmitMessageForUserHandler(serviceClient));
+app.post(
+  "/api/v1/messages",
+  getSubmitMessageForUserHandler(serviceClient, telemetryClient)
+);
 
 const azureFunctionHandler = createAzureFunctionHandler(app);
 
