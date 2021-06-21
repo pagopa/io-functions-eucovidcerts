c4e3ac8ac4a06c7968d5d2f97f1c0ff4c3746db3
Emanuele De Cupis <emanuele.decupis@pagopa.it>
2021-06-21 11:49:05 +0200

    Fix base45 error (#44)

--- GetCertificate/handler.ts
+++ GetCertificate/handler.ts
@@ -26,7 +26,7 @@ import { identity, toString } from "fp-ts/lib/function";
 import { PreferredLanguage } from "@pagopa/io-functions-commons/dist/generated/definitions/PreferredLanguage";
 import { Context } from "@azure/functions";
 import { TypeofApiCall } from "@pagopa/ts-commons/lib/requests";
-import { Either, toError } from "fp-ts/lib/Either";
+import { Either, toError, right } from "fp-ts/lib/Either";
 import { Validation } from "io-ts";
 import * as o from "fp-ts/lib/Option";
 import { StatusEnum } from "../generated/definitions/ValidCertificate";
@@ -37,7 +37,7 @@ import { GetCertificateByAutAndCFT } from "../generated/dgc/requestTypes";
 import { SearchSingleQrCodeResponseDTO } from "../generated/dgc/SearchSingleQrCodeResponseDTO";
 import { toSHA256 } from "../utils/conversions";
 import { createDGCClientSelector } from "../utils/dgcClientSelector";
-import { parseQRCode } from "./parser";
+import { parseQRCode, parseQRCodeAlt } from "./parser";
 import { printDetails, printInfo, printUvci } from "./printer";
 
 const assertNever = (x: never): never => {
@@ -128,20 +128,35 @@ export const GetCertificateHandler = (
       })
       // try to enhance raw certificate with parsed data
       .map(({ data: { qrcodeB64 = "", uvci } = {} }) => ({
-        printedCertificate: parseQRCode(qrcodeB64)
-          .mapLeft(_ => {
-            context.log.error(
-              `${logPrefix}|parseQRCode|unable to parse QRCode|${_.reason}`
-            );
-          })
-          .fold(
-            _ => undefined,
-            f => ({
-              detail: printDetails(selectedLanguage, f),
-              info: printInfo(selectedLanguage, f),
-              uvci: printUvci(selectedLanguage, f)
+        printedCertificate:
+          // We are experiencing unexpected failures while parsing on a small % of cases
+          // We want to deploy an alternative version of the parsing algo
+          //   as we cannot test on every possible data item that may occour in real life scenarios,
+          //   we keep the old implementation and use the new as fallback
+          parseQRCode(qrcodeB64)
+            .mapLeft(_ => {
+              context.log.error(
+                `${logPrefix}|parseQRCode|unable to parse QRCode|${_.reason}`
+              );
+              return _;
             })
-          ),
+            .fold(
+              _ => parseQRCodeAlt(qrcodeB64),
+              _ => right(_)
+            )
+            .mapLeft(_ => {
+              context.log.error(
+                `${logPrefix}|parseQRCode|unable to parse QRCode|Alt|${_.reason}`
+              );
+            })
+            .fold(
+              _ => undefined,
+              f => ({
+                detail: printDetails(selectedLanguage, f),
+                info: printInfo(selectedLanguage, f),
+                uvci: printUvci(selectedLanguage, f)
+              })
+            ),
         qrcodeB64,
         uvci
       }))
