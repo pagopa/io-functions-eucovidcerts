a61703af686848528d8c194a92e61f5f7c8bdda2
gquadrati <greta.quadrati@pagopa.it>
2021-07-09 10:02:27 +0200

    dataset cleanup based on new values

--- GetCertificate/valuesets/labTestManufactorers.ts
+++ GetCertificate/valuesets/labTestManufactorers.ts
@@ -5,38 +5,159 @@ import { VALUESET_PLACEHOLDER } from "./placeholder";
 
 export const labTestManufactorers: IReadonlyMap = {
   placeholder: VALUESET_PLACEHOLDER,
-  "1232": "Abbott Rapid Diagnostics, Panbio COVID-19 Ag Test",
+  "1833": "AAZ-LMB, COVID-VIRO",
+  "1232": "Abbott Rapid Diagnostics, Panbio COVID-19 Ag Rapid Test",
+  "1468": "ACON Laboratories, Inc, Flowflex SARS-CoV-2 Antigen rapid test",
+  "2108": "AESKU.DIAGNOSTICS GmbH & Co. KG, AESKU.RAPID SARS-CoV-2",
+  "2130": "Affimedix, Inc., TestNOW® - COVID-19 Antigen Test",
   "1304": "AMEDA Labordiagnostik GmbH, AMP Rapid Test SARS-CoV-2 Ag",
-  "1065": "Becton Dickinson, Veritor System Rapid Detection of SARS-CoV-2",
+  "1822":
+    "Anbio (Xiamen) Biotechnology Co., Ltd, Rapid COVID-19 Antigen Test(Colloidal Gold)",
+  "1815":
+    "Anhui Deep Blue Medical Technology Co., Ltd, COVID-19 (SARS-CoV-2) Antigen Test Kit (Colloidal Gold) - Nasal Swab",
+  "1736":
+    "Anhui Deep Blue Medical Technology Co., Ltd, COVID-19 (SARS-CoV-2) Antigen Test Kit(Colloidal Gold)",
+  "768": "ArcDia International Ltd, mariPOC SARS-CoV-2",
+  "1654": "Asan Pharmaceutical CO., LTD, Asan Easy Test COVID-19 Ag",
+  "2010":
+    "Atlas Link Technology Co., Ltd., NOVA Test® SARS-CoV-2 Antigen Rapid Test Kit (Colloidal Gold Immunochromatography)",
+  "1800": "AVALUN SAS, Ksmart® SARS-COV2 Antigen Rapid Test",
+  "2101":
+    "AXIOM Gesellschaft für Diagnostica und Biochemica mbH, COVID-19 Antigen Rapid Test",
+  "1906": "Azure Biotech Inc, COVID-19 Antigen Rapid Test Device",
+  "1065":
+    "Becton Dickinson, BD Veritor™ System for Rapid Detection of SARS CoV 2",
+  "1870":
+    "Beijing Hotgen Biotech Co., Ltd, Novel Coronavirus 2019-nCoV Antigen Test (Colloidal Gold)",
   "1331":
     "Beijing Lepu Medical Technology Co., Ltd, SARS-CoV-2 Antigen Rapid Test Kit",
   "1484":
     "Beijing Wantai Biological Pharmacy Enterprise Co., Ltd, Wantai SARS-CoV-2 Ag Rapid Test (FIA)",
+  "2031":
+    "Bio-Rad Laboratories / Zhejiang Orient Gene Biotech, Coronavirus Ag Rapid Test Cassette (Swab)",
+  "1599":
+    "Biomerica, Inc., Biomerica COVID-19 Antigen Rapid Test (nasopharyngeal swab)",
   "1242": "Bionote, Inc, NowCheck COVID-19 Ag Test",
-  "1223": "BIOSYNEX SWISS SA, BIOSYNEX COVID-19 Ag BSS",
-  "1173": "CerTest Biotec, S.L., CerTest SARS-CoV-2 Card test",
+  "1223": "BIOSYNEX S.A., BIOSYNEX COVID-19 Ag BSS",
+  "1494": "BIOSYNEX S.A., BIOSYNEX COVID-19 Ag+ BSS",
+  "1236": "BTNX Inc, Rapid Response COVID-19 Antigen Rapid Test",
+  "1173": "CerTest Biotec, CerTest SARS-CoV-2 Card test",
+  "1919": "Core Technology Co., Ltd, Coretests COVID-19 Ag Test",
+  "1581": "CTK Biotech, Inc, OnSite COVID-19 Ag Rapid Test",
+  "1225": "DDS DIAGNOSTIC, Test Rapid Covid-19 Antigen (tampon nazofaringian)",
+  "1375": "DIALAB GmbH, DIAQUICK COVID-19 Ag Cassette",
+  "1739": "Eurobio Scientific, EBS SARS-CoV-2 Ag Rapid Test",
+  "2147": "Fujirebio, ESPLINE SARS-CoV-2",
   "1244": "GenBody, Inc, Genbody COVID-19 Ag Test",
+  "2012": "Genrui Biotech Inc, SARS-CoV-2 Antigen Test Kit (Colloidal Gold)",
+  "1253": "GenSure Biotech Inc, GenSure COVID-19 Antigen Rapid Kit",
+  "2183":
+    "Getein Biotech, Inc., One Step Test for SARS-CoV-2 Antigen (Colloidal Gold)",
+  "1144": "Green Cross Medical Science Corp., GENEDIA W COVID-19 Ag",
+  "1747":
+    "Guangdong Hecin Scientific, Inc., 2019-nCoV Antigen Test Kit (colloidal gold method)",
   "1360": "Guangdong Wesail Biotech Co., Ltd, COVID-19 Ag Test Kit",
+  "1324":
+    "Guangzhou Decheng Biotechnology Co., LTD, V-CHEK, 2019-nCoV Ag Rapid Test Kit (Immunochromatography)",
+  "1437":
+    "Guangzhou Wondfo Biotech Co., Ltd, Wondfo 2019-nCoV Antigen Test (Lateral Flow Method)",
+  "1257": "Hangzhou AllTest Biotech Co., Ltd, COVID-19 Antigen Rapid Test",
+  "1610":
+    "Hangzhou Clongene Biotech Co., Ltd, COVID-19 Antigen Rapid Test Cassette",
   "1363": "Hangzhou Clongene Biotech Co., Ltd, Covid-19 Antigen Rapid Test Kit",
-  "1767":
-    "Healgen Scientific Limited Liability Company, Coronavirus Ag Rapid Test Cassette",
-
+  "1365":
+    "Hangzhou Clongene Biotech Co., Ltd, COVID-19/Influenza A+B Antigen Combo Rapid Test",
+  "1844":
+    "Hangzhou Immuno Biotech Co.,Ltd, Immunobio SARS-CoV-2 Antigen ANTERIOR NASAL Rapid Test Kit (minimal invasive)",
+  "1215":
+    "Hangzhou Laihe Biotech Co., Ltd, LYHER Novel Coronavirus (COVID-19) Antigen Test Kit(Colloidal Gold)",
+  "2139":
+    "HANGZHOU LYSUN BIOTECHNOLOGY CO., LTD., COVID-19 Antigen Rapid Test Device（Colloidal Gold）",
+  "1392":
+    "Hangzhou Testsea Biotechnology Co., Ltd, COVID-19 Antigen Test Cassette",
+  "1767": "Healgen Scientific, Coronavirus Ag Rapid Test Cassette",
+  "1263": "Humasis, Humasis COVID-19 Ag Test",
+  "2006":
+    "Jiangsu Medomics medical technology Co.,Ltd., SARS-CoV-2 antigen Test Kit (LFIA)",
   "1333":
     "Joinstar Biomedical Technology Co., Ltd, COVID-19 Rapid Antigen Test (Colloidal Gold)",
-  "1268": "LumiraDX UK Ltd, LumiraDx SARS-CoV-2 Ag Test",
+  "1764":
+    "JOYSBIO (Tianjin) Biotechnology Co., Ltd, SARS-CoV-2 Antigen Rapid Test Kit (Colloidal Gold)",
+  "1266": "Labnovation Technologies Inc, SARS-CoV-2 Antigen Rapid Test Kit",
+  "2128":
+    "Lumigenex (Suzhou) Co., Ltd, PocRoc®SARS-CoV-2 Antigen Rapid Test Kit (Colloidal Gold)",
+  "1267": "LumiQuick Diagnostics Inc, QuickProfile COVID-19 Antigen Test",
+  "1268": "LumiraDX, LumiraDx SARS-CoV-2 Ag Test",
   "1180": "MEDsan GmbH, MEDsan SARS-CoV-2 Antigen Rapid Test",
-  "1481": "MP Biomedicals Germany GmbH, Rapid SARS-CoV-2 Antigen Test Card",
+  "2029":
+    "Merlin Biomedical (Xiamen) Co., Ltd., SARS-CoV-2 Antigen Rapid Test Cassette",
+  "1775": "MEXACARE GmbH, MEXACARE COVID-19 Antigen Rapid Test",
+  "1190": "möLab, mö-screen Corona Antigen Test",
+  "1481": "MP Biomedicals, Rapid SARS-CoV-2 Antigen Test Card",
+  "2104": "Nal von minden GmbH, NADAL COVID -19 Ag +Influenza A/B Test",
   "1162": "Nal von minden GmbH, NADAL COVID-19 Ag Test",
+  "1420": "NanoEntek, FREND COVID-19 Ag",
+  "1501":
+    "New Gene (Hangzhou) Bioengineering Co., Ltd, COVID-19 Antigen Detection Kit",
+  "1199": "Oncosem Onkolojik Sistemler San. ve Tic. A.S., CAT",
+  "308": "PCL Inc, PCL COVID19 Ag Rapid FIA",
+  "2243": "PCL Inc., PCL COVID19 Ag Gold",
+  "2116":
+    "PerGrande BioTech Development Co., Ltd., SARS-CoV-2 Antigen Detection Kit (Colloidal Gold Immunochromatographic Assay)",
   "1271": "Precision Biosensor, Inc, Exdia COVID-19 Ag",
+  "1495": "Prognosis Biotech, Rapid Test Ag 2019-nCov",
   "1341":
     "Qingdao Hightop Biotech Co., Ltd, SARS-CoV-2 Antigen Rapid Test (Immunochromatography)",
   "1097": "Quidel Corporation, Sofia SARS Antigen FIA",
+  "1606": "RapiGEN Inc, BIOCREDIT COVID-19 Ag - SARS-CoV 2 Antigen test",
+  "1604": "Roche (SD BIOSENSOR), SARS-CoV-2 Rapid Antigen Test",
+  "2228": "Roche (SD BIOSENSOR), SARS-CoV-2 Rapid Antigen Test Nasal",
   "1489":
     "Safecare Biotech (Hangzhou) Co. Ltd, COVID-19 Antigen Rapid Test Kit (Swab)",
+  "1490":
+    "Safecare Biotech (Hangzhou) Co. Ltd, Multi-Respiratory Virus Antigen Test Kit(Swab)  (Influenza A+B/ COVID-19)",
+  "1201": "ScheBo Biotech AG, ScheBo SARS CoV-2 Quick Antigen",
   "344": "SD BIOSENSOR Inc, STANDARD F COVID-19 Ag FIA",
   "345": "SD BIOSENSOR Inc, STANDARD Q COVID-19 Ag Test",
+  "2052": "SD BIOSENSOR Inc, STANDARD Q COVID-19 Ag Test Nasal",
+  "1319": "SGA Medikal, V-Chek SARS-CoV-2 Ag Rapid Test Kit (Colloidal Gold)",
+  "1357": "SGA Medikal, V-Chek SARS-CoV-2 Rapid Ag Test (colloidal gold)",
+  "2109":
+    "Shenzhen Lvshiyuan Biotechnology Co., Ltd., Green Spring SARS-CoV-2 Antigen-Rapid test-Set",
+  "1967":
+    "Shenzhen Microprofit Biotech Co., Ltd, SARS-CoV-2 Antigen Test Kit (Colloidal Gold Chromatographic Immunoassay)",
+  "2017":
+    "Shenzhen Ultra-Diagnostics Biotec.Co.,Ltd, SARS-CoV-2 Antigen Test Kit",
+  "1769":
+    "Shenzhen Watmind Medical Co., Ltd, SARS-CoV-2 Ag Diagnostic Test Kit (Colloidal Gold)",
+  "1768":
+    "Shenzhen Watmind Medical Co., Ltd, SARS-CoV-2 Ag Diagnostic Test Kit (Immuno-fluorescence)",
+  "1574":
+    "Shenzhen Zhenrui Biotechnology Co., Ltd, Zhenrui ®COVID-19 Antigen Test Cassette",
   "1218": "Siemens Healthineers, CLINITEST Rapid Covid-19 Antigen Test",
+  "1114": "Sugentech, Inc, SGTi-flex COVID-19 Ag",
+  "1466": "TODA PHARMA, TODA CORONADIAG Ag",
+  "1934":
+    "Tody Laboratories Int., Coronavirus (SARS-CoV 2) Antigen - Oral Fluid",
+  "2074":
+    "Triplex International Biosciences (China) Co., LTD., SARS-CoV-2 Antigen Rapid Test Kit",
+  "1443": "Vitrosens Biotechnology Co., Ltd, RapidFor SARS-CoV-2 Rapid Ag Test",
+  "2103":
+    "VivaChek Biotech (Hangzhou) Co., Ltd, VivaDiag Pro SARS-CoV-2 Ag Rapid Test",
+  "2098":
+    "Wuhan EasyDiagnosis Biomedicine Co., Ltd., COVID-19 (SARS-CoV-2) Antigen Test Kit",
+  "2090":
+    "Wuhan UNscience Biotechnology Co., Ltd., SARS-CoV-2 Antigen Rapid Test Kit",
+  "1763":
+    "Xiamen AmonMed Biotechnology Co., Ltd, COVID-19 Antigen Rapid Test Kit (Colloidal Gold)",
   "1278": "Xiamen Boson Biotech Co. Ltd, Rapid SARS-CoV-2 Antigen Test Card",
+  "1456": "Xiamen Wiz Biotech Co., Ltd, SARS-CoV-2 Antigen Rapid Test",
+  "1884":
+    "Xiamen Wiz Biotech Co., Ltd, SARS-CoV-2 Antigen Rapid Test (Colloidal Gold)",
+  "1296":
+    "Zhejiang Anji Saianfu Biotech Co., Ltd, AndLucky COVID-19 Antigen Rapid Test",
+  "1295":
+    "Zhejiang Anji Saianfu Biotech Co., Ltd, reOpenTest COVID-19 Antigen Rapid Test",
   "1343":
-    "Zhejiang Orient Gene Biotech, Coronavirus Ag Rapid Test Cassette (Swab)"
+    "Zhezhiang Orient Gene Biotech Co., Ltd, Coronavirus Ag Rapid Test Cassette (Swab)"
 };
