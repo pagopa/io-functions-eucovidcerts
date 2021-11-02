7420f27faeb05b310c8d95900ec2c15887bedf6a
Emanuele De Cupis <emanuele.decupis@pagopa.it>
2021-11-02 18:24:08 +0100

    [#IP-435]  fix notify webhook (#74)

--- IOEventsWebhook/__tests__/handler.test.ts
+++ IOEventsWebhook/__tests__/handler.test.ts
@@ -1,10 +1,13 @@
 import { Context } from "@azure/functions";
-import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
+import { FiscalCode, NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 import { None } from "fp-ts/lib/Option";
 import * as TE from "fp-ts/lib/TaskEither";
+import { toSHA256 } from "../../utils/conversions";
 
 import { HealthCheck, HealthProblem } from "../../utils/healthcheck";
 import { IOEventsWebhookHandler } from "../handler";
+
+const aFiscalCode = "AAABBB00A00A000B" as FiscalCode;
 
 afterEach(() => {
   jest.clearAllMocks();
@@ -13,22 +16,22 @@ afterEach(() => {
 describe("IOEventsWebhookHandler", () => {
   it("should consume an expected event", async () => {
     const mockContext = ({ log: console, bindings: {} } as unknown) as Context;
-    const name = "service-subscribed" as NonEmptyString;
-    const payload = { hashedFiscalCode: "fakevalue" };
+    const name = "service:subscribed" as NonEmptyString;
+    const payload = { fiscalCode: aFiscalCode };
     const handler = IOEventsWebhookHandler();
 
     const response = await handler(mockContext, { name, payload });
 
     expect(response.kind).toBe("IResponseSuccessAccepted");
     expect(mockContext.bindings.outputFiscalCode).toBe(
-      payload.hashedFiscalCode
+      toSHA256(payload.fiscalCode)
     );
   });
 
   it("should not consume an unexpected event", async () => {
     const mockContext = ({ log: console, bindings: {} } as unknown) as Context;
     const name = "unexpected-event" as NonEmptyString;
-    const payload = { hashedFiscalCode: "fakevalue" };
+    const payload = { fiscalCode: aFiscalCode };
     const handler = IOEventsWebhookHandler();
 
     const response = await handler(mockContext, { name, payload });
@@ -42,7 +45,7 @@ describe("IOEventsWebhookHandler", () => {
       log: console,
       bindings: {}
     } as unknown) as Context;
-    const name = "service-subscribed" as NonEmptyString;
+    const name = "service:subscribed" as NonEmptyString;
     const payload = { wrongField: "fakevalue" };
     const handler = IOEventsWebhookHandler();
 
