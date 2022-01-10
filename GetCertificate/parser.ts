deb016d79fdb16d41d50f1a60babbd7137a7f25a
Emanuele De Cupis <emanuele.decupis@pagopa.it>
2022-01-10 13:14:03 +0100

    [#IC-173] Refactor Certificate parsing (#81)

--- GetCertificate/parser.ts
+++ GetCertificate/parser.ts
@@ -1,6 +1,6 @@
 import * as zlib from "pako";
 import { Either, toError } from "fp-ts/lib/Either";
-import * as e from "fp-ts/lib/Either";
+import * as E from "fp-ts/lib/Either";
 import { Errors } from "io-ts";
 import { PNG, PNGWithMetadata } from "pngjs";
 import jsQR from "jsqr";
@@ -8,6 +8,7 @@ import { QRCode } from "jsqr";
 import { readableReport } from "@pagopa/ts-commons/lib/reporters";
 import { match } from "ts-pattern";
 import { pipe } from "fp-ts/lib/function";
+import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 import {
   Certificates,
   RecoveryCertificate,
@@ -29,44 +30,44 @@ const removePrefix = (s: string): string =>
   s.startsWith("HC1:") ? s.substring(4) : s;
 
 const inflate = (b: Buffer): Either<Error, Uint8Array> =>
-  e.tryCatch(() => (b[0] === 0x78 ? zlib.inflate(b) : b), toError);
+  E.tryCatch(() => (b[0] === 0x78 ? zlib.inflate(b) : b), toError);
 
 interface IBorcDecoded {
   readonly value: ReadonlyArray<Buffer>;
 }
 
 const base64ToBuffer = (base64: string): Either<Error, Buffer> =>
-  e.tryCatch(() => Buffer.from(base64, "base64"), toError);
+  E.tryCatch(() => Buffer.from(base64, "base64"), toError);
 
 const bufferToPng = (buffer: Buffer): Either<Error, PNGWithMetadata> =>
-  e.tryCatch(() => PNG.sync.read(buffer), toError);
+  E.tryCatch(() => PNG.sync.read(buffer), toError);
 
 const pngToQrcode = (png: PNGWithMetadata): Either<Error, QRCode> =>
   pipe(
-    e.tryCatch(
+    E.tryCatch(
       () => jsQR(Uint8ClampedArray.from(png.data), png.width, png.height),
       toError
     ),
-    e.chain(
-      e.fromNullable(
+    E.chain(
+      E.fromNullable(
         new Error("can not find a valid qr code in the input image")
       )
     )
   );
 
 const base45Decode = (s: string): Either<Error, Buffer> =>
-  e.tryCatch(() => base45.decode(s), toError);
+  E.tryCatch(() => base45.decode(s), toError);
 
 const borcDecodeFirst = (bytes: Uint8Array): Either<Error, IBorcDecoded> =>
-  e.tryCatch(() => borc.decodeFirst(bytes), toError);
+  E.tryCatch(() => borc.decodeFirst(bytes), toError);
 
 const bordDecode = (
   b: Buffer
 ): Either<Error, ReadonlyMap<number, ReadonlyMap<number, unknown>>> =>
-  e.tryCatch(() => borc.decode(b), toError);
+  E.tryCatch(() => borc.decode(b), toError);
 
 const readHCert = <T>(m: ReadonlyMap<number, T>): Either<Error, T> =>
-  e.fromNullable(new Error("borc decode failed: missing -260 map entry"))(
+  E.fromNullable(new Error("borc decode failed: missing -260 map entry"))(
     m.get(-260)
   );
 
@@ -88,7 +89,7 @@ export const withTrace = <T, I>(
 ) => (i: I): Either<Error, T> =>
   pipe(
     fn(i),
-    e.mapLeft(error => {
+    E.mapLeft(error => {
       const message =
         error instanceof Error ? error.message : readableReport(error);
       return new Error(`step: ${stepName}, error: ${message}`);
@@ -109,7 +110,7 @@ export const decodeCertificateAndLogMissingValues = (
 
   pipe(
     decodedValude,
-    e.map(value => {
+    E.map(value => {
       pipe(
         match(value)
           .when(TestCertificate.is, te =>
@@ -122,12 +123,46 @@ export const decodeCertificateAndLogMissingValues = (
             getRecoveryCertificateValidationErrors(rc, x)
           )
           .exhaustive(),
-        e.mapLeft((err: string) => logWarning(`Missing map values|${err}`))
+        E.mapLeft((err: string) => logWarning(`Missing map values|${err}`))
       );
     })
   );
   return decodedValude;
 };
+
+// exported for testing purpose
+export const extractDataFromPng = (
+  rawPng: NonEmptyString
+): Either<Error, string> =>
+  pipe(
+    E.right(rawPng),
+    E.chain(withTrace(base64ToBuffer)),
+    E.chain(withTrace(bufferToPng)),
+    E.chain(withTrace(pngToQrcode)),
+    E.map(qr => qr.data)
+  );
+
+// exported for testing purpose
+export const decodeCertificateData = (logWarning: (warn: string) => void) => (
+  data: string
+): Either<Error, Certificates> =>
+  pipe(
+    E.right(data),
+    E.map(removePrefix),
+    E.chain(withTrace(base45Decode)),
+    E.chain(withTrace(inflate)),
+    E.chain(withTrace(borcDecodeFirst)),
+    E.map(cose => cose.value[2]),
+    E.chain(withTrace(bordDecode)),
+    E.chain(withTrace(readHCert)),
+    E.map(m => m.get(1)),
+    E.chain(
+      withTrace(
+        decodeCertificateAndLogMissingValues(logWarning),
+        "Certificates.decode"
+      )
+    )
+  );
 
 /**
  * Parse a qr code image to get Certificate information payload
@@ -140,25 +175,17 @@ export const parseQRCode = (
   logWarning: (warn: string) => void
 ): Either<IQRParsingFailure, Certificates> =>
   pipe(
+    // formal input validation
     qrcode,
-    e.fromNullable<Error>(new Error("can not decode an empty string")),
-    e.chain(withTrace(base64ToBuffer)),
-    e.chain(withTrace(bufferToPng)),
-    e.chain(withTrace(pngToQrcode)),
-    e.map(qr => qr.data),
-    e.map(removePrefix),
-    e.chain(withTrace(base45Decode)),
-    e.chain(withTrace(inflate)),
-    e.chain(withTrace(borcDecodeFirst)),
-    e.map(cose => cose.value[2]),
-    e.chain(withTrace(bordDecode)),
-    e.chain(withTrace(readHCert)),
-    e.map(m => m.get(1)),
-    e.chain(
-      withTrace(
-        decodeCertificateAndLogMissingValues(logWarning),
-        "Certificates.decode"
-      )
-    ),
-    e.mapLeft(_ => ({ qrcode, reason: _.message }))
+    NonEmptyString.decode,
+    E.mapLeft(_ => new Error("can not decode an empty string")),
+
+    // get data from encoded png string
+    E.chain(extractDataFromPng),
+
+    // parse certificate data
+    E.chain(decodeCertificateData(logWarning)),
+
+    // map an eventually occurred error
+    E.mapLeft(_ => ({ qrcode, reason: _.message }))
   );
