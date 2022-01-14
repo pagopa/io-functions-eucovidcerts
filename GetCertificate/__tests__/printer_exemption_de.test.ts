b3ffd831fefbc13143e9bc89ef05abbe307d7e3b
gquadrati <greta.quadrati@pagopa.it>
2022-01-14 09:55:43 +0100

    [#IC-189] add tests for exemption certificate

---
+++ GetCertificate/__tests__/printer_exemption_de.test.ts
@@ -0,0 +1,37 @@
+import { PreferredLanguageEnum } from "@pagopa/io-functions-commons/dist/generated/definitions/PreferredLanguage";
+import { getOrElseW } from "fp-ts/lib/Either";
+import { pipe } from "fp-ts/lib/function";
+import { some } from "fp-ts/lib/Option";
+
+import { Certificates } from "../certificate";
+import { printDetails, printInfo } from "../printer";
+
+import { aValidExemptionCertificate } from "../../__mocks__/certificates";
+
+const preferredLanguage = PreferredLanguageEnum.de_DE;
+
+describe(`Printer - Exemption - ${preferredLanguage}`, () => {
+  it("should print its info page", () => {
+    const certificate = pipe(
+      Certificates.decode(aValidExemptionCertificate),
+      getOrElseW(_ => {
+        throw "Error decoding object";
+      })
+    );
+
+    const result = printInfo(some(preferredLanguage), certificate);
+    expect(result).toMatchSnapshot();
+  });
+
+  it("should print its detail page", () => {
+    const certificate = pipe(
+      Certificates.decode(aValidExemptionCertificate),
+      getOrElseW(_ => {
+        throw "Error decoding object";
+      })
+    );
+
+    const result = printDetails(some(preferredLanguage), certificate);
+    expect(result).toMatchSnapshot();
+  });
+});
