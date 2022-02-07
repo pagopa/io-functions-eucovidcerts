290eca046019e06b9c86374c20f5a67141365c92
gquadrati <greta.quadrati@pagopa.it>
2022-02-07 11:29:33 +0100

    [#IC-250] add new test for booster vaccination type

--- GetCertificate/__tests__/printer_vaccine_detail_en.test.ts
+++ GetCertificate/__tests__/printer_vaccine_detail_en.test.ts
@@ -5,43 +5,52 @@ import {
 import { getOrElseW } from "fp-ts/lib/Either";
 import { pipe } from "fp-ts/lib/function";
 import { some } from "fp-ts/lib/Option";
+import { aValidVaccinationCertificate } from "../../__mocks__/certificates";
 
 import { Certificates } from "../certificate";
 import { printDetails } from "../printer";
 
-describe("Printer - Test - Detail - en_GB", () => {
+const language = PreferredLanguageEnum.en_GB;
+
+describe(`Printer - Test - Detail - ${language}`, () => {
   it("should print its markdown", () => {
     const certificate = pipe(
-      Certificates.decode({
-        ver: "1.0.0",
-        nam: {
-          fn: "Di Caprio",
-          fnt: "DI<CAPRIO",
-          gn: "Marilù Teresa",
-          gnt: "MARILU<TERESA"
-        },
-        dob: "1977-06-16",
-        v: [
-          {
-            tg: "840539006",
-            vp: "1119349007",
-            mp: "EU/1/20/1528",
-            ma: "ORG-100030215",
-            dn: 2,
-            sd: 2,
-            dt: "2021-04-10",
-            co: "IT",
-            is: "IT",
-            ci: "01ITE7300E1AB2A84C719004F103DCB1F70A#6"
-          }
-        ]
-      }),
+      Certificates.decode(aValidVaccinationCertificate),
       getOrElseW(_ => {
         throw "Error decoding object";
       })
     );
 
-    const result = printDetails(some(PreferredLanguageEnum.en_GB), certificate);
+    const result = printDetails(some(language), certificate);
     expect(result).toMatchSnapshot();
   });
+
+  it.each`
+    doseNumber | totalDoses
+    ${3}       | ${2}
+    ${3}       | ${3}
+    ${2}       | ${1}
+  `(
+    "should print markdown in case of booster $doseNumber/$totalDoses",
+    ({ doseNumber, totalDoses }) => {
+      const certificate = pipe(
+        Certificates.decode({
+          ...aValidVaccinationCertificate,
+          v: [
+            {
+              ...aValidVaccinationCertificate.v[0],
+              dn: doseNumber,
+              sd: totalDoses
+            }
+          ]
+        }),
+        getOrElseW(_ => {
+          throw "Error decoding object";
+        })
+      );
+
+      const result = printDetails(some(language), certificate);
+      expect(result).toMatchSnapshot();
+    }
+  );
 });
